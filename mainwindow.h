28f778920361d3290bfeb8b6cca11c83354c5bbf
lauramia99 <halasziklauramia@gmail.com>
2026-04-24 11:18:12 +0200

    Initial Qt project setup

---
+++ mainwindow.h
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class QPlainTextEdit;
+
+
+// QT_BEGIN_NAMESPACE
+// namespace Ui {
+// class MainWindow;
+// }
+// QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+
+private:
+};
+#endif // MAINWINDOW_H
